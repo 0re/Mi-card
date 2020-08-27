3b51718ca6a771a229dc5b98d8fafaf52c95c360
Fanyi Olabimpe <faniyiolabimpe@gmail.com>
2020-08-27 15:39:49 +0100

    Add files via upload
    created a container widget

--- main.dart
+++ main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/image.jpg'),
-                ),
+              ),
                 Text(
                   'olabimpe',
                   style: TextStyle(
@@ -24,8 +27,62 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-            ]
-          )
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal:25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10.0
+                      ),
+                      Text(
+                        '08091086269',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      Text(
+                        'Faniyiolabimpe@gmail.com',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                        )
+                      )
+                    ],
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
       
